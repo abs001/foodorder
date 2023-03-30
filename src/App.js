5e66bd55b2fd49dd0c4eb6a70e44739f8ab74502
abs001 <abhijitkumbhar001@gmail.com>
2023-03-30 10:58:33 +0530

    Created Cart component, Added Modal via react portal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import Cart from './components/Cart/Cart';
 import Header from './components/Layouts/Header.js';
 import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <React.Fragment>
+        <Cart/>
         <Header/>
         <main>
             <Meals/>
